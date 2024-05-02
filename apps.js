4b0abcb255197e7d67c16e4f600c89a198fc3f37
claudiamendes <1ribev81@solent.ac.uk>
2024-05-02 22:17:22 +0100

    updated index.html

--- apps.js
+++ apps.js
@@ -1,14 +1,11 @@
-
 const express = require('express');
 
+//create an express instance
 const app = express();
 
 //listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p>home page</p>');
-});
-
-app.get('/', (req, res) => {
     res.sendFile('./views/index.html', { root: __dirname });
+});
